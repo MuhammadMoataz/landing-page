9e0da13ddce87eeba5b114330ff27abfbdf3e880
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-08-24 21:23:41 +0200

    Update app.js
    Active section is the only one to have the active class in the HTML

--- js/app.js
+++ js/app.js
@@ -73,13 +73,13 @@ function checkViewPort()
 {
 	console.log("hi");
 	let messageText = false;
-	for(let i = 0; i < sectionEl.length; i++)
-	{
+	for(let i = 0; i < sectionEl.length; i++) {
 		messageText = isInViewport(sectionEl[i]);
-		if(messageText)
-		{
+		if(messageText) {
 			sectionEl[i].parentElement.classList.add('your-active-class');
-			if (i > 0) sectionEl[i - 1].parentElement.classList.remove('your-active-class');
+			for (let j = 0; j < sectionEl.length; j++) {
+				if (j !== i) sectionEl[j].parentElement.classList.remove('your-active-class');
+			}
 		}
 	}
 }
@@ -89,8 +89,8 @@ function isInViewport(element) {
 	return (
 		rect.top >= 0 &&
 		rect.left >= 0 &&
-		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + element.offsetHeight &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth) + element.offsetWidth
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
 }
 
