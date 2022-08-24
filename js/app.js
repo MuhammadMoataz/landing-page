6fa2a7c7cb14ec4e09662c53aa4a44e35a5f2bc0
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-08-24 21:13:00 +0200

    JS Done

--- js/app.js
+++ js/app.js
@@ -25,7 +25,6 @@
  */
 const ulEl = document.getElementById("navbar__list");
 const sectionEl = document.querySelectorAll('.landing__container');
-// const addingList = document.createElement('ul');
 const navbar = document.querySelector('.navbar__menu');
 let len = sectionEl.length;
 
@@ -72,25 +71,26 @@ make_nav();
 
 function checkViewPort()
 {
+	console.log("hi");
 	let messageText = false;
-	for(let i = 0; i < len; i++)
+	for(let i = 0; i < sectionEl.length; i++)
 	{
 		messageText = isInViewport(sectionEl[i]);
 		if(messageText)
 		{
 			sectionEl[i].parentElement.classList.add('your-active-class');
-			if(i != 0){sectionEl[i-1].parentElement.classList.remove('your-active-class');}
+			if (i > 0) sectionEl[i - 1].parentElement.classList.remove('your-active-class');
 		}
 	}
 }
 
-function isInViewport(section) {
-	const rect = section.getBoundingClientRect();
+function isInViewport(element) {
+	const rect = element.getBoundingClientRect();
 	return (
 		rect.top >= 0 &&
 		rect.left >= 0 &&
-		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + element.offsetHeight &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth) + element.offsetWidth
 	);
 }
 
@@ -105,9 +105,7 @@ document.addEventListener("mousemove", checkViewPort);
 
 // Scroll to section on link click
 const navSectionEl = document.querySelectorAll('.href-elements');
-console.log(navSectionEl[0]);
 for (let i = 0; i < sectionEl.length; i++) {
-	// addingList.children[i].addEventListener('click', function (e) {1
 	navSectionEl[i].addEventListener('click', function (e){
 		e.preventDefault();
 		sectionEl[i].scrollIntoView({behavior: "smooth"});
