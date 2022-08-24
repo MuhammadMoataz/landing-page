d8bc7dbc7f9f40ad15f71534866029bc3550a50b
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-08-24 20:15:13 +0200

    Flash resolved

--- js/app.js
+++ js/app.js
@@ -23,11 +23,10 @@
  * Define Global Variables
  *
  */
-// const ulEl = document.getElementById("navbar__list");
+const ulEl = document.getElementById("navbar__list");
 const sectionEl = document.querySelectorAll('.landing__container');
 const addingList = document.createElement('ul');
 const navbar = document.querySelector('.navbar__menu');
-var length = sectionEl.length;
 
 
 /**
@@ -43,22 +42,24 @@ var length = sectionEl.length;
  */
 
 // build the nav
-function make_nav() {
+const make_nav = () => {
 	for (let i = 1; i <= sectionEl.length; i++) {
-		const liEl = document.createElement('li');
-		liEl.innerHTML = `<a href="" class="href-elements">Section ${i}</a>`;
-		// ulEl.innerHTML += `<!--<li class="navSection"><a href="" class="href-elements">Section ${i}</a></li>-->`;
-		addingList.appendChild(liEl);
+		// const liEl = document.createElement('li');
+		// liEl.innerHTML = `<a href="" class="href-elements">Section ${i}</a>`;
+		ulEl.innerHTML += `<li class="navSection"><a href="" class="href-elements">Section ${i}</a></li>`;
+		// addingList.appendChild(liEl);
 	}
-	navbar.appendChild(addingList);
+	// navbar.appendChild(addingList);
 }
 make_nav();
 
 const navSectionEl = document.querySelectorAll('.href-elements');
 console.log(navSectionEl[0]);
-for (let i = 0; i < length; i++) {
-	addingList.children[i].addEventListener('click', function () {
-		sectionEl[i].scrollIntoView();
+for (let i = 0; i < sectionEl.length; i++) {
+	// addingList.children[i].addEventListener('click', function (e) {
+	navSectionEl[i].addEventListener('click', function (e){
+		e.preventDefault();
+		sectionEl[i].scrollIntoView({behavior: "smooth"});
 	})
 }
 
@@ -81,3 +82,30 @@ for (let i = 0; i < length; i++) {
 
 // Set sections as active
 
+const checkViewPort = () => {
+	let messageText = false;
+	for(let i = 0; i < length ; i++)
+	{
+		// check if the section in on focus
+		messageText = isInViewport(sectionEl[i]);
+		if(messageText)
+		{
+			sectionEl[i].parentElement.classList.add('your-active-class');
+			if(i != 0){sectionEl[i-1].parentElement.classList.remove('your-active-class');}
+		}
+	}
+}
+
+const isInViewport = (el) => {
+	const rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+// getting view port to work
+document.addEventListener("scroll", checkViewPort);
+document.addEventListener("mousemove", checkViewPort);
